45cfcb96645f55003f24b9bac11f7a73ed8f93d3
Jochen Joswig <jochen.joswig@maibornwolff.de>
2023-11-12 13:24:48 +0100

    improve sankey chart

--- sankey.py
+++ sankey.py
@@ -6,12 +6,13 @@ fig = go.Figure(data=[go.Sankey(
       thickness = 20,
       line = dict(color = "black", width = 0.5),
       label = ["Tourism Technology Festival", "The Keep Eco Rooms", "❌ no accommodation", "Hostel One", "7 Burgers"],
-      color = "blue"
+      # color = ['rgba(),']
     ),
     link = dict(
-      source = [0, 0, 0, 1, 3], # indices correspond to labels, eg A1, A2, A1, B1, ...
-      target = [1, 2, 3, 4, 4],
-      value = [4, 80, 40, 4, 7]
+      source = [0, 0, 0, 0, 1, 3, 2], # indices correspond to labels, eg A1, A2, A1, B1, ...
+      target = [1, 1, 2, 3, 4, 4, 4],
+      value = [3, 1, 8, 4, 4, 2, 2]
+      # color = []
   ))])
 
 fig.update_layout(title_text="Basic Sankey Diagram", font_size=10)
